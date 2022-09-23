d9952bda50213edb62116c70dc1544e45019d83d
tarakish <akr.3838soccer14@gmail.com>
2022-09-23 23:16:26 +0900

    Feat: github認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  helper_method :logged_in?
+
+  private
+  def logged_in?
+    !!session[:user_id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
