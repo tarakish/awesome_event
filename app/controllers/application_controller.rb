5f8b1e0c933367120388f9fdefc7cca4ba9dbc6a
tarakish <akr.3838soccer14@gmail.com>
2022-09-25 01:52:14 +0900

    Feat: authenticateメソッドの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
   helper_method :logged_in?
 
   private
   def logged_in?
     !!session[:user_id]
+  end
+
+  def current_suer
+    return unless session[:user_id]
+    @current_user = ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: 'ログインしてください'
   end
 end
