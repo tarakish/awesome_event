a82d65510a8d9e7d4f973c60d259064087bbc3a3
tarakish <akr.3838soccer14@gmail.com>
2022-09-26 16:50:30 +0900

    Feat: イベント参加機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :events
   root 'welcome#index'
   get '/auth/:provider/callback', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+
+  resources :events do
+    resources :tickets
+  end
 end
