79def93d8d4502ed9296473dca908b52000272ed
tarakish <akr.3838soccer14@gmail.com>
2022-09-26 18:20:52 +0900

    Feat: 退会機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :logout
 
+  resource :retirements
+
   resources :events do
     resources :tickets
   end
