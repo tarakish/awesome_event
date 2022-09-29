74e46da03a597c9d17fb334404536b3fa5fff816
tarakish <akr.3838soccer14@gmail.com>
2022-09-30 00:56:01 +0900

    Feat: ActiveStorageのバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,10 @@ class Event < ApplicationRecord
   validates :name, length: { maximum: 50 }, presence: true
   validates :place, length: { maximum: 100 }, presence: true
   validates :content, length: { maximum: 2000 }, presence: true
+  validates :image,
+    content_type: [:png, :jpg, :jpeg],
+    size: { less_than_or_equal_to: 10.megabytes },
+    dimension: { width: { max: 2000 }, height: { max: 2000 } }
   validates :start_at, presence: true
   validates :end_at, presence: true
   validate :start_at_should_be_before_end_at
